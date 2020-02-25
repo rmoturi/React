6736f20efe172471d7913c85c2f85dda24f9452f
rekha moturi <rmoturi@gmail.com>
2020-02-25 06:35:02 -0600

    AutoComplete with Hooks and CSS

--- src/components/forms/AutoCompleteSearch.js
+++ src/components/forms/AutoCompleteSearch.js
@@ -1,78 +1,76 @@
-import React, { Component } from 'react';
+import React, { useState, useEffect } from 'react';
+import './AutoComplete.css';
 
-class AutoCompleteSearch extends Component {
-    constructor(props){
-        super(props);
-        this.state = {
-            value:'',
-            suggestions:[]
+const AutoCompleteSearch = () => {
+    const [display, setDisplay] = useState(false);
+    const [searchValue, setSearchValue] = useState('');
+    const [suggestions, setSuggestions] = useState([]);
+
+    useEffect(() => {
+        if (searchValue && !searchValue.trim()) {
+            return ;
         }
-    }
-    
-    onChange = e => {
-        let searchValue = e.target.value;
-        const suggestionsArray = ["Luke","Laksh","Luck","Obi","R2","Me"];
-        console.log(suggestionsArray);
-        console.log(searchValue);
-        if (!searchValue.trim()) {
-          this.setState({ suggestions: [] })
-          return ;
-        }
-  
-        if (searchValue.length >= 1) {
-            this.setState({ value : searchValue, suggestions: suggestionsArray.filter((a) => a.toLowerCase().startsWith(searchValue.toLowerCase())) });
-        }
-    }
-
-    searchCharacterByName(text){
-        const response =  fetch("https://swapi.co/api/people");
-        return response;
-    };
-
-    selectItemFromList(suggestionObject) {
-        this.setState({
-          value: suggestionObject.name ,
-          suggestions: [suggestionObject]
-        });
-        console.log(this);
-      }
-
-    render() {
         
-        let renderItems = () => {
-            let listItems = this.state.suggestions.map((suggestion, index) => {
-            let suggestionObject = suggestion;
-                return (<li key={suggestionObject}>{suggestionObject}</li>
-                //<li onClick={() => this.selectItemFromList(suggestionObject)} key={index}>
-                //{suggestionObject.name}
-                //</li>
+        if (searchValue.length >= 1) {
+            setSearchValue(searchValue);
+            async function getCharacters(){
+                const response = await fetch("https://swapi.co/api/people");
+                const body = await response.json();
+                console.log(body);
+                setSuggestions(
+                    body.results.filter(({name}) => name.toLowerCase().startsWith(searchValue.toLowerCase()))
+                    .map(({name}) => ({label: name, value: name}))
                 );
-            });
+                console.log(suggestions);
+
+            }
+            getCharacters();
+        }
+    }, [searchValue]);
+
+    // function searchCharacterByName(text) {
+    //     const response =  fetch("https://swapi.co/api/people");
+    //     return response;
+    // };
+
+    const selectItemFromList = (suggestionObject) => {
+        setSearchValue(suggestionObject.label);
+        setDisplay(false);
+    }
+      
+    let renderItems = () => {
+        let listItems = suggestions.map((suggestion, index) => {
+            let suggestionObject = suggestion;
             return (
-            <div>
-                <ul className="doctor-list">
+                <li onClick={() => selectItemFromList(suggestionObject)} key={index}>
+                    {suggestionObject.label}
+                </li>
+            );
+        });
+        return (
+            <div className="autocomplete dropdown">
+                <ul>
                 {listItems}
                 </ul>
             </div>
-            );
-
-        }
-        console.log(this.state.value);
-
-        return (
-            <div>
-                <input 
-                type="text" 
-                id="characterSearch" 
-                placeholder="start typing a character name" 
-                onChange={this.onChange} 
-                value={this.state.value}
-                ></input>
-                {this.state.value}
-                {this.state.suggestions?.length > 0 ? renderItems() : null}
-            </div>
         );
     }
+
+    return (
+        <div>
+            <input 
+            type="text" 
+            id="characterSearch" 
+            placeholder="start typing a character name" 
+            onClick={() => setDisplay(!display)}
+            onChange={(e) => setSearchValue(e.target.value)} 
+            value={searchValue}
+            ></input>
+            
+            {suggestions?.length > 0 ? renderItems() : null}
+            <span>Search Text: {searchValue}</span>
+        </div>
+    );
 }
 
 export default AutoCompleteSearch;
